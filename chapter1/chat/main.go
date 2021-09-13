38a0575e705b97d4b1c1dfd9fe406f9bee390493
haru <haruki6180@gmail.com>
2021-09-13 11:26:48 +0900

    ログの記録を無効化

--- chapter1/chat/main.go
+++ chapter1/chat/main.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"chat/trace"
 	"flag"
 	"log"
 	"net/http"
-	"os"
 	"path/filepath"
 	"sync"
 	"text/template"
@@ -31,7 +29,7 @@ func main() {
 	var addr = flag.String("addr", ":8080", "アプリケーションのアドレス")
 	flag.Parse()
 	r := newRoom()
-	r.tracer = trace.New(os.Stdout)
+	// r.tracer = trace.New(os.Stdout) でターミナルにログを出力
 	http.Handle("/", &templateHandler{filename: "chat.html"})
 	http.Handle("/room", r)
 	// チャットルームを開始します
